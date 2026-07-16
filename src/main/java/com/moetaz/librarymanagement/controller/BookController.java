2d6c362cb5b583debcfc189f9adbe4a68b44c2a8
Moetaz Troje <moetaz.trojet@gmail.com>
2026-07-16 10:20:36 +0100

    Implement GET /books endpoint

--- src/main/java/com/moetaz/librarymanagement/controller/BookController.java
+++ src/main/java/com/moetaz/librarymanagement/controller/BookController.java
@@ -5,6 +5,8 @@ import com.moetaz.librarymanagement.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class BookController {
 
@@ -15,7 +17,7 @@ public class BookController {
     }
 
     @GetMapping("/books")
-    public Book getBook() {
-        return bookService.getBook();
+    public List<Book> getBooks() {
+        return bookService.getBooks();
     }
 }
--- src/main/java/com/moetaz/librarymanagement/service/BookService.java
+++ src/main/java/com/moetaz/librarymanagement/service/BookService.java
@@ -2,15 +2,19 @@ package com.moetaz.librarymanagement.service;
 
 import com.moetaz.librarymanagement.model.Book;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BookService {
 
-    public Book getBook() {
-        return new Book(
-                1,
-                "His las bow",
-                "Conan Doyle"
-        );
+    public List<Book> getBooks() {
+        final List<Book> books = new ArrayList<Book>();
+        books.add(new Book(1, "His last bow", "Conan doyle"));
+        books.add(new Book(2, "The shadow of the wind", "Ruiz Zafon"));
+        books.add(new Book(3, "A dance with dragons", "George RR Martin"));
+
+        return books;
     }
-}
+
+    }
