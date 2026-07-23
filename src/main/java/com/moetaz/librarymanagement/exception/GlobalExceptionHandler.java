12b56a8022c3b8439281d7853b58c720a05a4b44
Moetaz Troje <moetaz.trojet@gmail.com>
2026-07-23 19:04:57 +0100

    Implement more clear  global exception handling and validation responses

--- src/main/java/com/moetaz/librarymanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/moetaz/librarymanagement/exception/GlobalExceptionHandler.java
@@ -1,16 +1,55 @@
 package com.moetaz.librarymanagement.exception;
 
 
+import org.hibernate.annotations.CurrentTimestamp;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import com.moetaz.librarymanagement.dto.ErrorResponse;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
-    public ResponseEntity<String> handleBookNotFound(BookNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleBookNotFound(BookNotFoundException ex) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse error = new ErrorResponse
+                (       LocalDateTime.now(),
+                        status.value(),
+                        status.getReasonPhrase(),
+                        ex.getMessage());
+        return ResponseEntity
+                .status(status)
+                .body(error);
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationError(MethodArgumentNotValidException ex){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        Map<String, String> validationErrors = new HashMap<>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
+        }
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                "Request validation failed",
+                validationErrors
+        );
+
+        return ResponseEntity
+                .status(status)
+                .body(error);
+    }
+
 }
+
+
+
